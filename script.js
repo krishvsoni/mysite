fea82a57fcbe7e33b2418df8050fc7a2371433cf
KRISH SONI <67964054+krishvsoni@users.noreply.github.com>
2023-05-28 16:35:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+  
+  // Get form values
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+  
+  // You can perform further validation or data processing here
+  
+  // Send the form data to a server or perform any desired action
+  // For this example, let's log the form data to the console
+  console.log('Name:', name);
+  console.log('Email:', email);
+  console.log('Message:', message);
+  
+  // Optionally, display a success message or redirect the user to a thank you page
+  alert('Message sent successfully!');
+  
+  // Clear the form fields
+  document.getElementById('
 
-window.onload = function() {
-  // Add an event listener to the "Contact Me" link.
-  document.querySelector("a[href='mailto:email@address.com']").addEventListener("click", function() {
-    // Open the user's email client.
-    window.open("mailto:email@address.com");
-  });
-};
