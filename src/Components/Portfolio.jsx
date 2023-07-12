60bd5155c3af3b5d35d8008a816ab34d7fae1d9a
krishvsoni <sonikrish994@gmail.com>
2023-07-12 23:44:06 +0530

    commit

---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,94 @@
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+
+import React from "react";
+
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "../images/design-desk.jpeg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "Carbon Footprint",
+    description:
+      "Calculate the carbon emission from the given url.",
+    url: "https://github.com/krishvsoni/Carbon-footprint",
+  },
+  {
+    title: "AI-Sha",
+    description:
+      "AI-Sha an OpenAI based chatbot.",
+    url: "https://github.com/krishvsoni/AI-Sha",
+  },
+  {
+    title: "TypeSpace",
+    description:
+      "Tkinter based notepad.",
+    url: "https://github.com/krishvsoni/TypeSpace",
+  },
+  {
+    title: "Diabetes Prediction Dataset",
+    description:
+      "Diabetes Prediction on Pandas and Numpy",
+    url: "https://github.com/krishvsoni/Diabetes-Prediction-Dataset",
+  },
+  {
+    title: "TextUtility",
+    description:
+      "TextUtlity removes punctuations in your text , removes extra lines , removes new lines and capitalized your full text . Visit us at textutility.online",
+    url: "https://github.com/krishvsoni/TextUtility",
+  },
+
+
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+              </a>
+              <p className="small">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
